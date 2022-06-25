58134fce827bb57d2eef6b800731e78941674742
Ave K <ssj_100@yahoo.com>
2022-06-26 06:15:42 +0800

    fix: cleanup

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,20 +1,12 @@
 import { Button } from '@mui/material'
 import React, { FC, useState } from 'react'
 import { ipfsService } from '../services/ipfs'
-//import styled from 'styled-components'
 
-
-/*
-const StyledButton = styled(Button)`
-  border: 1px solid #e9e9e9;
-  margin-left: auto;
-`
-*/
 
 const FileUpload: FC = () => {
 
     const [fileUpload, setFileUpload]= useState()
-    
+
     const handleChange = (e) => {
         setFileUpload(e.target.files[0])
     }
