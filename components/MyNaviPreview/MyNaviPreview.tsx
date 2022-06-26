c54818b663e7cc8feed8d4b12935a5f3595d96d4
rick <l.riccardo@web.de>
2022-06-26 07:05:36 -0400

    added image merge in base64 URI

--- components/MyNaviPreview/MyNaviPreview.tsx
+++ components/MyNaviPreview/MyNaviPreview.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React, { useState } from "react";
+import mergeImages from "merge-images";
 
 function MyNaviPreview({
   chain,
@@ -11,17 +12,42 @@ function MyNaviPreview({
   basicsTwo,
   basicsThree,
 }) {
+  let preview = mergeImages([
+    `/navi-attributes/basics/${basicsOne}.png`,
+    `/navi-attributes/basics/${basicsTwo}.png`,
+    `/navi-attributes/basics/${chain}.png`,
+    `/navi-attributes/basics/${proficiency}.png`,
+    `/navi-attributes/basics/${handRight}.png`,
+    `/navi-attributes/basics/${handLeft}.png`,
+    `/navi-attributes/basics/${basicsThree}.png`,
+    `/navi-attributes/basics/${group}.png`,
+  ]).then((b64) => (document.querySelector("img").src = b64));
+
+  console.log("preview:", preview);
+
   return (
-    <div className="font-mont pt-10">
-      <div>{basicsOne}</div>
-      <div>{basicsTwo}</div>
-      <div>{chain}</div>
-      <div>{proficiency}</div>
-      <div>{handRight}</div>
-      <div>{handLeft}</div>
-      <div>{basicsThree}</div>
-      <div>mouth</div>
-      <div>{group}</div>
+    <div>
+      <div className="font-mont pt-10">
+        <div>{basicsOne}</div>
+        <div>{basicsTwo}</div>
+        <div>{chain}</div>
+        <div>{proficiency}</div>
+        <div>{handRight}</div>
+        <div>{handLeft}</div>
+        <div>{basicsThree}</div>
+        <div>mouth</div>
+        <div>{group}</div>
+      </div>
+
+      <div>
+        {/* <Image
+          src={`/navi-attributes/basics/${group}.png`}
+          alt="Picture of the author"
+          width={500}
+          height={500}
+        /> */}
+        {/* <img src={preview}></img> */}
+      </div>
     </div>
   );
 }
