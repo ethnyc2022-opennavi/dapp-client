097d2983fcc502cac3af2479416764ce01b4e30d
rick <l.riccardo@web.de>
2022-06-26 08:30:42 -0400

    added new images

--- components/MyNaviPreview/MyNaviPreview.tsx
+++ components/MyNaviPreview/MyNaviPreview.tsx
@@ -11,10 +11,12 @@ function MyNaviPreview({
   basicsOne,
   basicsTwo,
   basicsThree,
+  basicsFour,
 }) {
   let preview = mergeImages([
     `/navi-attributes/basics/${basicsOne}.png`,
     `/navi-attributes/basics/${basicsTwo}.png`,
+    `/navi-attributes/basics/${basicsFour}.png`,
     `/navi-attributes/basics/${proficiency}.png`,
     `/navi-attributes/basics/${chain}.png`,
     `/navi-attributes/basics/${handRight}.png`,
